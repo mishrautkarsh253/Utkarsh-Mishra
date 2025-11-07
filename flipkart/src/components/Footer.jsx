5dd4ebdedd8d685c86ecbe505f8607be86d02c65
Utkarsh Mishra <mishrautkarsh253@gmail.com>
2025-11-07 10:10:14 +0530

    reactpjoject

---
+++ flipkart/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+       <ul className='head'>
+        <li>electronics</li>
+        <li>about</li>
+        <li>contact</li>
+        <li>help</li>
+      </ul>
+    </div>
+  )
+}
+
+export default F
---
+++ flipkart/src/components/Nevbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nevbar = () => {
+  return (
+    <div>
+      <ul className='nav'>
+        <li>home</li>
+        <li>about</li>
+        <li>contact</li>
+        <li>help</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Nevbar
